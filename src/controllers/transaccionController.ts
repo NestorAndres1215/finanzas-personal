9ac1e6a3d21985fe83967de61fa77483613ef593
NestorAndres1215 <atiroandres@gmail.com>
2026-02-14 02:16:13 -0500

    subiendo

--- src/controllers/transaccionController.ts
+++ src/controllers/transaccionController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { TransaccionService } from "../services/transaccionservice";
+import { TransaccionService } from "../services/transaccionService";
 
 // LISTAR
 export const listarTransacciones = async (req: Request, res: Response) => {
