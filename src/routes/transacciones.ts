da95369ca1296f561f60e6bc8ff6de214b3a0492
NestorAndres1215 <atiroandres@gmail.com>
2026-02-14 02:23:30 -0500

    subiendo

--- src/routes/transacciones.ts
+++ src/routes/transacciones.ts
@@ -1,10 +1,28 @@
 import { Router } from "express";
-import { listarTransacciones, crearTransaccion, verGraficos } from "../controllers/transaccionController";
+import {
+  listarTransacciones,
+  crearTransaccion,
+  verGraficos,
+  listarPorCategoria,
+  listarPorTipo,
+  listarPorFecha,
+  listarPorRangoFechas,
+  verTransaccion,
+  actualizarTransaccion,
+  eliminarTransaccion
+} from "../controllers/transaccionController";
 
 const router = Router();
 
 router.get("/", listarTransacciones);
 router.post("/", crearTransaccion);
-
 router.get("/graficos", verGraficos);
+router.get("/categoria/:categoria", listarPorCategoria);
+router.get("/tipo/:tipo", listarPorTipo);
+router.get("/fecha", listarPorFecha); 
+router.get("/rango", listarPorRangoFechas);
+router.get("/:id", verTransaccion);
+router.post("/:id/actualizar", actualizarTransaccion);
+router.post("/:id/eliminar", eliminarTransaccion);
+
 export default router;
